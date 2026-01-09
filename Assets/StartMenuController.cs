e2c934c5bcb75c5d519d61c3a4c4c18b0b00f9d1
Sella2000 <a25sebno@student.his.se>
2026-01-09 08:49:09 +0100

    StartScreen

---
+++ Assets/StartMenuController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuController : MonoBehaviour
+{
+    public void OnStartClick()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+
+}
