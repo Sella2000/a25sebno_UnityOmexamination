270c08c8c5bb5f004842fd20a958a570b8e6cd42
Sella2000 <a25sebno@student.his.se>
2026-01-09 11:08:28 +0100

    animationParticlesandcollection

---
+++ Assets/Game2.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game2 : MonoBehaviour
+{
+    public void OnStartClick()
+    {
+        SceneManager.LoadScene("Game2");
+    }
+
+
+}
